f0a7027e0eb754a6f0d8bc07244720904c56adcc
unknown <vutuanthai.hcm@gmail.com>
2024-01-17 20:04:25 +0700

    dua ma nguon

---
+++ nha.py
@@ -0,0 +1,24 @@
+import turtle
+t = turtle.Turtle()
+# nhac ngoi viet len
+t.penup()
+# di chuyen den buoc can ve
+t.goto(-200,200)
+# ve hinh vuong
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+# xoay con tro 45do
+t.left(45)
+t.forward(80)
+# xoay con tro x
+t.right(100)
+t.forward(80)
+# ve hinh tron
+t.penup()
+t.
