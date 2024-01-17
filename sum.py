28d3ec7404f3f83e1a178b168e6f898db0659ab1
unknown <vutuanthai.hcm@gmail.com>
2024-01-17 21:25:43 +0700

    toan tu

---
+++ sum.py
@@ -0,0 +1,7 @@
+# input A,B
+A = 5
+B = 7
+# xu ly
+C = A + B
+# xuat
+print(C)
---
+++ toan_tu_so_sanh.py
@@ -0,0 +1,12 @@
+# 2 gia tri bang nhau hay khong == cho true false
+# != so sanh hai gia tri co khac nhau hay khong
+# > so sanh gia tri lon hon
+# < so sanh nho hon
+# <= 
+# >=
+a = 5
+b = 5
+print(a==b)
+c = 10
+d = 5
+print(c!=d)
